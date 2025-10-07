847129bd3cb983370ca68fd07e544169b5a3661a
mariemamdouni <mariemamdouni555@gmail.com>
2025-10-07 11:14:53 +0100

    prosit5

---
+++ Prosits-Java-prosit4/src/tn/esprit/gestionzoo/main/ZooManagement.java
@@ -0,0 +1,74 @@
+package tn.esprit.gestionzoo.main;
+
+import tn.esprit.gestionzoo.entities.*;
+
+public class ZooManagement {
+
+    public static void main(String[] args) {
+        Animal lion = new Animal();
+        lion.setName("Simba");
+        lion.setAge(8);
+        lion.setFamily("Cats");
+        lion.setMammal(true);
+
+        Zoo myZoo = new Zoo("Wildlife Park", "Ariana");
+        Zoo notMyZoo = new Zoo("WaterPark", "Siliana");
+
+
+        Animal dog = new Animal("Canine", "Snoopy", 2, true);
+
+
+        System.out.println(myZoo.addAnimal(lion));
+        System.out.println(myZoo.addAnimal(dog));
+
+        myZoo.displayAnimals();
+
+        System.out.println(myZoo.searchAnimal(dog));
+        Animal dog2 = new Animal("Canine", "lll", 2, true);
+        System.out.println(myZoo.searchAnimal(dog2));
+
+//           System.out.println(myZoo.removeAnimal(dog));
+        myZoo.displayAnimals();
+
+
+        System.out.println(myZoo);
+
+        myZoo.addAnimal(lion);
+        myZoo.addAnimal(dog);
+        myZoo.addAnimal(dog2);
+        myZoo.displayAnimals();
+
+
+        myZoo.setName("Belvedere Park");
+        Zoo notMyZoo1 = Zoo.comparerZoo(myZoo, notMyZoo);
+        System.out.println(notMyZoo1);
+
+
+
+
+        /*Aquatic aquatic = new Aquatic();
+        Terrestrial terrestrial = new Terrestrial();
+        Dolphin dolphin = new Dolphin();
+        Penguin penguin = new Penguin();*/
+
+        Aquatic a = new Aquatic("Aquatic family", "Poisson", 2, false, "Bassin");
+        Terrestrial t = new Terrestrial("Mammifères", "Chien", 4, true, 4);
+        Dolphin d = new Dolphin("Cétacés", "Flipper", 8, true, "Océan", 25.5f);
+        Penguin p = new Penguin("Oiseaux", "Pingou", 3, false, "Mer", 12.0f);
+
+        System.out.println("Objets créés avec constructeurs paramétrés ");
+
+        System.out.println(a);
+        System.out.println(t);
+        System.out.println(d);
+        System.out.println(p);
+
+
+        a.swim();
+        d.swim();
+        p.swim();
+
+
+    }
+
+}
